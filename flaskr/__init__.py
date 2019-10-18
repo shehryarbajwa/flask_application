c11c8c590159b8228cbe5d22fab4bdfe66448673
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-10-18 17:57:58 +0500

    initialized flask cors app

---
+++ flaskr/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return jsonify({'message': 'HELLO WORLD'})
+
+    return app

2c7170abce9448ebb27d8cc93abc4f9d813fac02
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-10-18 20:17:22 +0500

    utilize CORS to fetch cross domain objects

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, Response, redirect
+from flask_cors import CORS
 
 def create_app(test_config=None):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='pages')
+    CORS(app)
 
-    @app.route('/')
-    def hello():
-        return jsonify({'message': 'HELLO WORLD'})
+
+    @app.route('/shehryar')
+    def index():
+        return jsonify([{"city": "Lahore",
+        "state": "Punjab",
+        "venues": [{
+            "id": 1,
+            "name": "The Musical Hop",
+            "num_upcoming_shows": 0},
+            {
+                "id": 2,
+                "name": "Park Square Live Music",
+                "num_upcoming_shows": 1
+            }]
+        }])
+
+    @app.route('/api')
+    def api_route():
+        return render_template('index.html')
 
     return app

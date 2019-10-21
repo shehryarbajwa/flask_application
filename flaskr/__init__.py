a753e18925d838c2be39a439209e2f882e7e31e6
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-10-21 20:04:12 +0500

    added db logic

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,18 @@
-from flask import Flask, jsonify, render_template, request, Response, redirect
+from flask import Flask, jsonify, render_template, request, Response, redirect, abort
+from .models import setup_db, Plant
 from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__, template_folder='pages')
+    setup_db(app)
     CORS(app)
+
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
 
     @app.route('/shehryar')
@@ -16,7 +25,7 @@ def create_app(test_config=None):
             "num_upcoming_shows": 0},
             {
                 "id": 2,
-                "name": "Park Square Live Music",
+                "name": "Gaddafi Stadium",
                 "num_upcoming_shows": 1
             }]
         }])
@@ -24,5 +33,39 @@ def create_app(test_config=None):
     @app.route('/api')
     def api_route():
         return render_template('index.html')
+
+    
+    @app.route('/plants', methods=['GET','POST'])
+    def get_plants():
+        page = request.args.get('page', 1, type=int)
+        print(page)
+        start = (page - 1) * 10
+        print(start)
+        end = start + 5
+        print(end)
+        plants = Plant.query.all()
+        formatted_plants = [plant.format() for plant in plants]
+
+
+        return jsonify({
+            'success': True,
+            'plants': formatted_plants[start:end],
+            'total_plants': len(formatted_plants) 
+        })
+    
+    @app.route('/plants/<int:plant_id>')
+    def get_specific_plant(plant_id):
+        plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+
+        if plant is None:
+            abort(404)
+
+        else:
+
+            return jsonify({
+                'success': True,
+                'plant': plant.format()
+            })
+
 
     return app

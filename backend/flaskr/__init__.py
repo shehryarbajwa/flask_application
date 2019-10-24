b3e4c3b5acbcc3a6c16da907e971ba6eea5b5699
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-10-24 14:50:15 +0500

    flaskr up and running

---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,1 @@
+from .flaskr import create_app
---
+++ backend/tests/test_flaskr.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+import json
+from   flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from .models import setup_db, Book
+
+class BookCaseTest(unittest.TestCase):
+    """This class represents the trivia test case"""
+    
+    def setUp(self):
+        #Create the app
+        self.app = create_app()
+        #Allows the client to set up test_client for us to start testing the application
+        self.client = self.app.test_client
+        self.database_name = 'bookshelf_test'
+        self.database_path = 'postgres://{}@{}/{}'.format('shehryarbajwa', 'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        self.new_book = {
+            'title': 'Anansi boys',
+            'author' : 'Neil Gaiman',
+            'rating': 5
+        }
+
+    def tearDown(self):
+        pass
+
+    def test_get_paginated_books(self):
+        res = self.client().get('/books')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        #Assert true that there is a number of total_books. In case, there are no books, this will return None
+        self.assertTrue(data['total_books'])
+        #Assert true that there is a len of the value books. In case there are no books, this will return None
+        self.assertTrue(len(data["books"]))
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        #The json body is just to show whether there is any json
+        #In this case it is none so we dont really need it
+        # res = self.client().get('/books?page=1000', json={'rating': 1})
+        res = self.client().get('/books?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
